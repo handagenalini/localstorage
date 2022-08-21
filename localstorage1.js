18b442385ab1967ebcb139ad2740690088ee1256
handagenalini <111242818+handagenalini@users.noreply.github.com>
2022-08-21 09:24:29 +0530

    Update localstorage1.js

--- localstorage1.js
+++ localstorage1.js
@@ -26,22 +26,50 @@
         shoeNewUserOnScreen(obj);
 
     }
-    function showNewUserOnScreen(user){
-        const parentNode=document.getElementById('listOfUser');
-        const childHTML=`<li id=${user.emailid}>${user.name} - ${user.emailid}
-            <button onclick=deleteUser('${user.emailid}')deleteUser</li>`
-        parentNode.innerHTML=parentNode.innerHTML+childHTML
-    }
-    function deleteUser(emailid){
-        console.log(emailid);
-        localStorage.removeItem(emailid);
-        removeUserFromScreen(emailid);
-    }
-    function removeUserFromScreen(emailid){
-        const parentNode=document.getElementById('listOfUser');
-        const childNodeToBeDeleted=document.getElementById(emailid);
-        parentNode.removeChild(childNodeToBeDeleted)
-    }
+function showNewUserOnScreen(user){
+                document.getElementById('email').value = '';
+                document.getElementById('username').value = '';
+                document.getElementById('phonenumber').value ='';
+                // console.log(localStorage.getItem(user.emailId))
+                if(localStorage.getItem(user.email) !== null){
+                    removeUserFromScreen(user.email)
+                }
+
+                const parentNode = document.getElementById('listOfUsers');
+                const childHTML = `<li id=${user.email}> ${user.name} - ${user.email}
+                                        <button onclick=deleteUser('${user.email}')> Delete User </button>
+                                        <button onclick=editUserDetails('${user.email}','${user.name}','${user.phonenumber}')>Edit User </button>
+                                     </li>`
+
+                parentNode.innerHTML = parentNode.innerHTML + childHTML;
+            }
+
+            
+
+            function editUserDetails(emailId, name, phonenumber){
+
+                document.getElementById('email').value = emailId;
+                document.getElementById('username').value = name;
+                document.getElementById('phonenumber').value =phonenumber;
+
+                deleteUser(emailId)
+             }
+
+            
+
+            function deleteUser(emailId){
+            function removeUserFromScreen(emailId){
+                const parentNode = document.getElementById('listOfUsers');
+                const childNodeToBeDeleted = document.getElementById(emailId);
+
+                parentNode.removeChild(childNodeToBeDeleted)
+                if(childNodeToBeDeleted) {
+                    parentNode.removeChild(childNodeToBeDeleted)
+                }
+            }
+
+
+    
 </script>
 
 </body>
@@ -51,3 +79,6 @@
 
  
  
+
+
+               
